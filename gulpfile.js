0c2fcbf48bd76f75232cbf28b3e05a46ddc50ede
Nadia Pillard <ua9ond@yandex.ru>
2016-06-27 21:00:56 +0600

    добавлена система сборки

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,103 @@ var gulp = require("gulp");
 
 var server = require("browser-sync");
 
+var concat = require("gulp-concat");
+
+var uglify = require("gulp-uglify");
+
+var rename = require("gulp-rename");
+
+var copy = require("gulp-contrib-copy");
+
+var clean = require("gulp-contrib-clean");
+
+var csso = require("gulp-csso");
+
+var autoprefixer = require("autoprefixer");
+
+
+gulp.task("concat", function() {
+  gulp.src("./js/src/*.js")
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("build/js/"))
+});
+
+
+gulp.task("minjs", function(){
+  gulp.src("./js/src/*.js")
+  .pipe(uglify())
+  .pipe(rename("main.min.js"))
+  .pipe(gulp.dest("build/js/"))
+  
+});
+
+
+gulp.task("copyJslib", function() {
+  gulp.src("js/*.js")
+  .pipe(copy())
+  .pipe(gulp.dest("build/js"))
+});
+
+
+
+gulp.task("style", function(){
+  gulp.src("css/main.css")
+  /*.pipe(postcss([
+    autoprefixer({browsers: [
+      "last 1 version",
+      "last 2 Chrome versions",
+      "last 2 Firefox versions",
+      "last 2 Opera versions",
+      "last 2 Edge versions"            
+    ]})
+    
+    
+  ]))*/
+  
+  .pipe(gulp.dest("build/css"))
+  .pipe(csso())
+  .pipe(rename("style.min.css"))
+  .pipe(gulp.dest("build/css"))
+  .pipe(server.reload({stream: true}));
+});
+
+
+gulp.task("copyHtml", function() {
+  gulp.src("*.html")
+  .pipe(copy())
+  .pipe(gulp.dest("build"))
+});
+
+gulp.task("copyAssets", function() {
+  gulp.src("./assets/**")
+  .pipe(copy())
+  .pipe(gulp.dest("build/assets"))
+  gulp.src("./img/**")
+  .pipe(copy())
+  .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("clean", function () {
+  return gulp.src("build", {read: false})
+    .pipe(clean());
+});
+
 gulp.task("show", function(){
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     ui: false
   });
   
   
-  gulp.watch("*.html").on("change", server.reload);
-  gulp.watch("js/*.js").on("change", server.reload);
+  gulp.watch("*.html", ["copyHtml"]).on("change", server.reload);
+  gulp.watch("js/src/*.js", ["minjs"]).on("change", server.reload);
   
 });
+
+gulp.task("build", ["clean", "copyHtml", "copyAssets", "concat", "minjs", "copyJslib", "style"]);
+
+
 
 
